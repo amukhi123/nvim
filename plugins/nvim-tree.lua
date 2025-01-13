912dd288daabd9921c2915e3ff70b676de74cadb
Aqil Mukhi <aqilmukhi123@gmail.com>
2025-01-12 16:53:20 -0800

    Disabled git integration for nvim tree

--- plugins/nvim-tree.lua
+++ plugins/nvim-tree.lua
@@ -7,7 +7,11 @@ return {
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
 
-        require("nvim-tree").setup()
+        require("nvim-tree").setup({
+            git = {
+                enable = false
+            }
+        })
 
         vim.keymap.set("n", "<C-l>", ":NvimTreeOpen<CR>", {silent = true})
         vim.keymap.set("n", "<C-A-l>", ":NvimTreeClose<CR>", {silent = true})
